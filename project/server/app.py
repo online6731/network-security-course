760d694745dee78a20c938968cffe4e29281e7ea
online6731 <mohammad.parsian@gmail.com>
2020-02-08 10:19:03 +0000

    argparse added with --interface

--- project/server/app.py
+++ project/server/app.py
@@ -1,5 +1,6 @@
 from api import app
 from sniffer import Sniffer
+import argparse
 
 
 server_socket_ip = '0.0.0.0'
@@ -7,5 +8,10 @@ server_socket_port = 5000
 sniffing_interface = 'enp3s0'
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--interface')
+    args = parser.parse_args()
+    sniffing_interface = args.interface if args.interface else sniffing_interface
+
     Sniffer().run(interface=sniffing_interface)
     socketio.run(app)
